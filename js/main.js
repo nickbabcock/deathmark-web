45f1a3d284ccf3caf4383b3e1b4c943fcc9b62c0
Nick Babcock <nbabcock19@hotmail.com>
2014-04-28 16:58:35 -0400

    Init commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function() {
+	'use strict';
+
+	var paras = $('footer p');
+	for (var i = 0; i < paras.length; i++) {
+		var p = $(paras[i]);
+		p.html(p.html().replace(':', ':<br />'));
+	};
+})
