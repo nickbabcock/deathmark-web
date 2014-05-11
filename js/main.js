fdd51e4ec029406e30b9d4e34f5830de36354852
Nick Babcock <nbabcock19@hotmail.com>
2014-05-11 10:04:33 -0500

    Don't rewrite urls to include br

--- js/main.js
+++ js/main.js
@@ -4,6 +4,6 @@ $(function() {
 	var paras = $('footer p');
 	for (var i = 0; i < paras.length; i++) {
 		var p = $(paras[i]);
-		p.html(p.html().replace(':', ':<br />'));
+		p.html(p.html().replace(/:\s+/, ':<br />'));
 	};
 })
